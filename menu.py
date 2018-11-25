47e6ea989c7edde6579ace938acbaa9811ce705f
Dave Rosinski <drosinski@outlook.com>
2018-11-25 07:16:44 -0600

    Fixed email not working issue

--- menu.py
+++ menu.py
@@ -100,13 +100,13 @@ def gconnect():
     print data
     login_session['username'] = data['name']
     login_session['picture'] = data['picture']
-    #login_session['email'] = data['email']
+    login_session['email'] = data['email']
 
     # Check if the user is in the database already
-    #user_id = getUserID(login_session['email'])
-    #if not user_id:
-    #    user_id = createUser(login_session)
-    #login_session['user_id'] = user_id
+    user_id = getUserID(login_session['email'])
+    if not user_id:
+        user_id = createUser(login_session)
+    login_session['user_id'] = user_id
 
     output = ''
     output += '<h1>Welcome, '
